4b3d3e1b71f352214a8e8425f0238ea322573b81
Abdul Muheeth <abdul.muheeth@smartdocs.ai>
2023-06-08 12:44:13 +0530

    unauthorized exception handling

--- src/main/java/com/xapp/xjava/config/SecurityConfig.java
+++ src/main/java/com/xapp/xjava/config/SecurityConfig.java
@@ -11,8 +11,6 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.AuthenticationEntryPoint;
-import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
@@ -24,13 +22,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private JwtAuthenticationFilter jwtFilter;
+
+    @Autowired
+    private JwtAuthenticationEntryPoint entryPoint;
  
     @Override
     protected void configure(HttpSecurity http) throws Exception {
        http.cors().disable().csrf().disable().authorizeHttpRequests()
        .antMatchers("/users/**").hasAuthority("ADMIN")
        .antMatchers("/**").permitAll()
-       .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+       .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+       .and().exceptionHandling().authenticationEntryPoint(entryPoint);
    
        http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
     }
